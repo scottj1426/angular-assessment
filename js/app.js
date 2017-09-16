0ac5da175aa9e61e61140ded9ee3d0fc413a7daf
James Scott <scottj1426@gmail.com>
2017-09-15 19:51:20 -0500

    Angular

--- js/app.js
+++ js/app.js
@@ -3,28 +3,28 @@ angular
   .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state("home", {
       url: "/",
-      templateUrl: "/js/views/home.html"
+      templateUrl: "./js/views/home.html"
     });
     $stateProvider.state("about", {
       url: "/about",
-      templateUrl: "/js/views/about.html"
+      templateUrl: "./js/views/about.html"
     });
     $stateProvider.state("blog", {
       url: "/blog",
-      templateUrl: "/js/views/blog.html"
+      templateUrl: "./js/views/blog.html"
     });
     $stateProvider.state("shop", {
       url: "/shop",
-      templateUrl: "/js/views/shop.html",
+      templateUrl: "./js/views/shop.html",
       controller: "shopCtrl"
     });
     $stateProvider.state("logo", {
       url: "/logo",
-      templateUrl: "/js/views/logo.html"
+      templateUrl: "./js/views/logo.html"
     });
     $stateProvider.state("detail", {
       url: "/productDetail",
-      templateUrl: "/js/views/product-details.html",
+      templateUrl: "./js/views/product-details.html",
       controller: "shopCtrl"
     });
 
